ca357952fb3e02ad78b96aa0ed78632b33c361eb
Ed Gow <elgow@uw.edu>
2016-12-21 17:08:30 -0800

    added  contains functions and unit tests

--- fabtools/edit.py
+++ fabtools/edit.py
@@ -8,71 +8,89 @@ Tools for performing batch edits of files on the target system. Implemended usin
 from pipes import quote
 import re
 from pathlib2 import Path
+from fabric.api import *
 
 _IN_MEMORY = '1h;2,$H;$!d;g'  # sed magic to do in-memory processing for multiline patterns
 
-def _mk_sed_cmd(op, files, args=None, start=None, end=None, opts=None, delim='+',  **kwargs):
+def _mk_sed_cmd(op, files, args=None, start=None, end=None, opts=None, inmem=False,  **kwargs):
     '''
-    Create the command string for running sed on the target system
+    Create the command string for running sed on the target system. If multiple files are specified then they will
+    be concatenated unless the '-s' option is specified.
     :param op: sed operation
-    :param file: target file path as Path object
+    :param files: target file(s) path as Path object or list of Paths.
     :param start: first address pattern (int, string, or regex. String will be regex escaped)
     :param end: second address pattern (int, string, or regex. String will be regex escaped)
     :param opts: extra sed opts
+    :param inmem: if true then pull file into memory and apply pattern and edits accross all lines
     :param args: sed operator args
     :param kwargs:
     :return: fully prepared shell command for sed operation
     '''
 
-    assert op in list('aiscd')
+    assert op in list('aiscd=')
     assert isinstance(files,Path) or type(files) is list
 
+
     files = files if type(files) is list else [files]
-    return 'sed {opts} -e {cmd} {args}'.format(
-        opts=quote('-' + opts) if opts else '',
-        cmd=quote("{start}{end}{op}".format (
-            start=_mk_selector(start, delim=delim),
-            end=',' + _mk_selector(end, delim=delim) if end else '',
-            op=op)),
+    return "sed -r {opts} {inmem} -e '{cmd}' {args} {files}".format(
+        opts=' '.join(opts) if opts else '',
+        inmem="-e '%s'" % _IN_MEMORY if inmem else '',
+        cmd='{start}{end}{op}'.format (
+            start=_mk_selector(start),
+            end=',%s' % _mk_selector(end) if end else '',
+            op=op),
         args=' '.join([quote(x) for x in args]) if args else '',
         files=' '.join([quote(str(x)) for x in list(files)])
     )
 
 
-def contains_line(pat, all=False):
+def contains_line(pat, files, use_sudo=False):
     '''
-    Checks for the presence of line(s) that match the pattern
+    Checks for the presence of line(s) that match the pattern, which cannot contain '\\n'. If multiple files are
+    specified then they will be concatenated unless the '-s' option is specified.
     :param pat: literal string or compiled regex
+    :param files: files to search
     :return: line number(s) if found, None if not
     '''
-    pass
+    try:
+        res = int(_run_func(use_sudo)(_mk_sed_cmd('=', files, start=pat, opts=['-n'])))
+        return res
+    except ValueError as e:
+        return None
 
 
-def contains(pat, end_pat):
+def contains(pat, files, use_sudo=False):
     '''
-    Search for text matching the pattern, which may span multiple lines by using '\\n'
+    Search for text matching the pattern, which may span multiple lines by using '\\n'. If multiple files are
+    specified then they will be concatenated unless the '-s' option is specified.
     :param pat: literal string or compiled regex
+    :param files: files to search
     :return: 1 if found, None if not
     '''
-    pass
+    try:
+        res = int(_run_func(use_sudo)(_mk_sed_cmd('=', files, start=pat, opts=['-n'], inmem=True)))
+        return res
+    except ValueError as e:
+        return None
 
 
-def prepend(text, pat=1, do_all=False):
+def prepend(text, files, pat=1, bak='', do_all=False, use_sudo=False):
     '''
     Prepend text ahead of line matching pattern [defaults to first line]. If do_all is true then every line matching
-    the pattern will be processed.
+    the pattern will be processed. This function acts on each specified file independently and in-place.
     :param text: text to insert
     :param pat: pattern to match
-    :param do_all:
+    :param bak: if not empty then a backup file will be created with this extension.
+    :param do_all: prepend text before every occurrance of the pattern
     :return:
     '''
-    pass
+    _run_func(use_sudo)(_mk_sed_cmd('=', files, start=pat, opts=['-n'], inmem=True))
 
 
 def append(text, pat='$', all=False):
     '''
     Prepend text ahead of line matching pattern [defaults to first line]. If do_all is true then every line matching
-    the pattern will be processed.
+    the pattern will be processed. This function acts on each specified file independently and in-place.
     :param text:
     :param pat:
     :param do_all:
@@ -99,21 +117,46 @@ def replace(pat, text, all=False):
     pass
 
 
+def _captured_local(*args, **kwargs):
+    return local(*args, capture=True, **kwargs)
 
+
+def _run_func(sudo_arg):
+    '''
+    Resolve the appropriate run function for fabric to use for normal(run()), sudo, or local execution
+    :param sudo_arg:
+    :return: the run function
+    '''
+    if not sudo_arg:
+        return run
+    elif sudo_arg is True:
+        return sudo
+    elif sudo_arg is local:
+        return _captured_local
+    elif sudo_arg in [run, sudo]:
+        return sudo_arg
 
 
 _REGEX_TYPE = type(re.compile('foo'))
 
-def _mk_selector(sel, delim='+'):
+def _mk_selector(sel):
     if sel is None:
         return ''
     elif type(sel) is int:
         return str(sel)
     elif type(sel) is str:
-        return '{0}{1}{0}'.format(delim, re.escape(sel))
+        pat = re.escape(sel)
+        return '\\{0}{1}{0}'.format(_choose_delim(pat), pat)
     elif type(sel) is _REGEX_TYPE:
-        return '{0}{1}{0}'.format(delim, sel.pattern)
+        pat = sel.pattern
+        return '\\{0}{1}{0}'.format(_choose_delim(pat), pat)
     else:
         raise TypeError('Illegal type for pattern %s' % str(type(sel)))
 
 
+_DELIM_CHARS = {'@', '#', '/', '_'}
+def _choose_delim(pat):
+    delims = _DELIM_CHARS - set(str(pat))
+    if not delims:
+        raise RuntimeError("No usable delimiter characters for pattern %s" % pat)
+    return sorted(list(delims))[0]
--- fabtools/tests/test_edit.py
+++ fabtools/tests/test_edit.py
@@ -2,19 +2,19 @@ from __future__ import absolute_import
 import unittest
 import re
 from pathlib2 import Path
+from fabric.api import run, sudo, local
 
 class EditTestCase(unittest.TestCase):
     def test__mk_selector(self):
         from fabtools.edit import _mk_selector
         _TEST_REGEX = 'a .ell of [aA] re.*x'
-        _DELIM = '%'
 
         # int
         self.assertEqual(_mk_selector(1234), '1234')
         # literal string
-        self.assertEqual(_mk_selector(_TEST_REGEX), "+" + re.escape(_TEST_REGEX) + "+")
+        self.assertEqual(_mk_selector(_TEST_REGEX), "\\#" + re.escape(_TEST_REGEX) + "#")
         # regex and delimiter
-        self.assertEqual(_mk_selector(re.compile(_TEST_REGEX), delim=_DELIM),  _DELIM + _TEST_REGEX + _DELIM)
+        self.assertEqual(_mk_selector(re.compile(_TEST_REGEX)),  '\\#' + _TEST_REGEX + '#')
 
 
     def test__mk_sed_cmd(self):
@@ -24,3 +24,37 @@ class EditTestCase(unittest.TestCase):
         self.assertEqual(_mk_sed_cmd('a', Path('/tmp/tryit'), 'FFFF'), '')
 
         # op, file, start=None, end=None, opts=None, delim='+', *args, **kwargs):
+
+
+    def test_contains_line(self):
+        from fabtools.edit import contains_line
+        import re
+
+        self.assertTrue(contains_line('one', Path('/tmp/tryit'), use_sudo=local))
+        self.assertFalse(contains_line('not one', Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains_line(re.compile('o[na]e'), Path('/tmp/tryit'), use_sudo=local))
+        self.assertFalse(contains_line(re.compile('o..e'), Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains_line(re.compile('^two$'), Path('/tmp/tryit'), use_sudo=local))
+
+
+    def test_contains(self):
+        from fabtools.edit import contains
+        import re
+
+        self.assertTrue(contains('one', Path('/tmp/tryit'), use_sudo=local))
+        self.assertFalse(contains('not one', Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains('one\ntwo\nthree', Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains(re.compile('t[whre\\n]+e'), Path('/tmp/tryit'), use_sudo=local))
+        self.assertFalse(contains(re.compile('o..e'), Path('/tmp/tryit'), use_sudo=local))
+
+
+    def test__choose_delim(self):
+        from fabtools.edit import _choose_delim
+
+        self.assertEqual(_choose_delim('hi'), '#')
+        self.assertEqual(_choose_delim('hi#'), '/')
+        self.assertEqual(_choose_delim('hi#/'), '@')
+        with self.assertRaises(RuntimeError):
+            _choose_delim('#/_@')
+
+
