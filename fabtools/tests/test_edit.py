2eaa15cf06490279067f347d3cbd84d65bf85d81
elgow <elgow@uw.edu>
2016-12-21 22:21:14 -0800

    add setup/teardown to unit tests

--- fabtools/tests/test_edit.py
+++ fabtools/tests/test_edit.py
@@ -3,8 +3,19 @@ import unittest
 import re
 from pathlib2 import Path
 from fabric.api import run, sudo, local
+from tempfile import NamedTemporaryFile
 
 class EditTestCase(unittest.TestCase):
+    def setUp(self):
+        with NamedTemporaryFile(delete=False) as dat:
+            self.textfile = Path(dat.name)
+            dat.write('one\ntwo\nthree\nfour\nfive\nsix\n')
+
+
+    def tearDown(self):
+        self.textfile.unlink()
+
+
     def test__mk_selector(self):
         from fabtools.edit import _mk_selector
         _TEST_REGEX = 'a .ell of [aA] re.*x'
@@ -21,7 +32,7 @@ class EditTestCase(unittest.TestCase):
         from fabtools.edit import _mk_sed_cmd
         print str(Path)
 
-        self.assertEqual(_mk_sed_cmd('a', Path('/tmp/tryit'), 'FFFF'), '')
+        self.assertEqual(_mk_sed_cmd('a', self.textfile, 'FFFF'), '')
 
         # op, file, start=None, end=None, opts=None, delim='+', *args, **kwargs):
 
@@ -30,22 +41,22 @@ class EditTestCase(unittest.TestCase):
         from fabtools.edit import contains_line
         import re
 
-        self.assertTrue(contains_line('one', Path('/tmp/tryit'), use_sudo=local))
-        self.assertFalse(contains_line('not one', Path('/tmp/tryit'), use_sudo=local))
-        self.assertTrue(contains_line(re.compile('o[na]e'), Path('/tmp/tryit'), use_sudo=local))
-        self.assertFalse(contains_line(re.compile('o..e'), Path('/tmp/tryit'), use_sudo=local))
-        self.assertTrue(contains_line(re.compile('^two$'), Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains_line('one', self.textfile, use_sudo=local))
+        self.assertFalse(contains_line('not one', self.textfile, use_sudo=local))
+        self.assertTrue(contains_line(re.compile('o[na]e'), self.textfile, use_sudo=local))
+        self.assertFalse(contains_line(re.compile('o..e'), self.textfile, use_sudo=local))
+        self.assertTrue(contains_line(re.compile('^two$'), self.textfile, use_sudo=local))
 
 
     def test_contains(self):
         from fabtools.edit import contains
         import re
 
-        self.assertTrue(contains('one', Path('/tmp/tryit'), use_sudo=local))
-        self.assertFalse(contains('not one', Path('/tmp/tryit'), use_sudo=local))
-        self.assertTrue(contains('one\ntwo\nthree', Path('/tmp/tryit'), use_sudo=local))
-        self.assertTrue(contains(re.compile('t[whre\\n]+e'), Path('/tmp/tryit'), use_sudo=local))
-        self.assertFalse(contains(re.compile('o..e'), Path('/tmp/tryit'), use_sudo=local))
+        self.assertTrue(contains('one', self.textfile, use_sudo=local))
+        self.assertFalse(contains('not one', self.textfile, use_sudo=local))
+        self.assertTrue(contains('one\ntwo\nthree', self.textfile, use_sudo=local))
+        self.assertTrue(contains(re.compile('t[whre\\n]+e'), self.textfile, use_sudo=local))
+        self.assertFalse(contains(re.compile('o..e'), self.textfile, use_sudo=local))
 
 
     def test__choose_delim(self):
